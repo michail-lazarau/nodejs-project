d5b936ee1f4158bbbdf120812a109174d6ff851a
Mikhail Lazarau <m.lazarau@godeltech.com>
2024-03-20 15:54:04 +0100

    moving error handling middlewares to src

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
-require('dotenv').config({path: "./.env"})
+require('dotenv').config({ path: './.env' });
 
-const express = require('express')
-const nunjucks = require('nunjucks')
-const bodyParser = require('body-parser')
-const asteroidsRoutes = require('./src/routes/meteors')
-const marsRoverPhotosRoutes = require('./src/routes/photos')
-const { errorHandler, notFoundHandler } = require('./ErrorHandlingMiddlewares/errorHandler')
-const path = require('path')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const asteroidsRoutes = require('./src/routes/meteors');
+const marsRoverPhotosRoutes = require('./src/routes/photos');
+const {
+  errorHandler,
+  notFoundHandler,
+} = require('./src/ErrorHandlingMiddlewares/errorHandler');
+const path = require('path');
 
-const { PORT } = process.env
-const app = express()
+const { PORT } = process.env;
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public'))) // styles
+app.use(express.static(path.join(__dirname, 'public'))); // styles
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine('html', nunjucks.render)
-app.set('view engine', 'html')
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
 
 nunjucks.configure(path.join(__dirname, 'views'), {
   autoescape: false,
   express: app,
-})
+});
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Header', 'Content-Type, Authorization');
   // res.setHeader('Access-Control-Allow-Header', 'Content-Type,  application/json')
-  next()
-})
+  next();
+});
 
-app.use(asteroidsRoutes)
+app.use(asteroidsRoutes);
 
-app.use(marsRoverPhotosRoutes)
+app.use(marsRoverPhotosRoutes);
 
-app.use(notFoundHandler)
+app.use(notFoundHandler);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const server = app.listen(parseInt(PORT), 'localhost', () => {
-  const { port } = server.address()
-  console.log(`Express app is running on port: ${port}`) // IPv6
-})
+  const { port } = server.address();
+  console.log(`Express app is running on port: ${port}`); // IPv6
+});
